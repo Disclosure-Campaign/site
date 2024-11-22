d64c2b5ab19d25b4cc8143b5a1d6e560cfb9d24f
Justin <jposer1@gmail.com>
2024-11-22 10:58:28 -0500

    added version note

--- src/pages/aboutPage.jsx
+++ src/pages/aboutPage.jsx
@@ -3,7 +3,10 @@ import TextPage from 'components/textPage';
 import aboutText from 'static/aboutText';
 
 const About = () => (
-  <TextPage uninterpolatedText={aboutText}/>
+  <div>
+    <TextPage uninterpolatedText={aboutText}/>
+    <div>Version 1.2</div>
+  </div>
 );
 
 export default About;
